e60f6a7b2713ceca22b59a6f70212cd324ea4dbe
Andrew Joslin <andrewtjoslin@gmail.com>
2016-03-19 17:51:19 -0700

    Use writeFileSync to write to out file
    Using a write stream, the target file would be erased each time
    css-extract started.
    
    This meant that, when using livereload with CSS, there were two visible
    reloads: once at the start of css-extract, and once when it finished.
    
    Using writeFileSync, the file is only edited once in place.

--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 const browserify = require('browserify')
+const tmpDir = require('tmp').dir
 const path = require('path')
 const test = require('tape')
 const bl = require('bl')
@@ -27,6 +28,30 @@ test('css-extract', function (t) {
         const expected = fs.readFileSync(exPath, 'utf8').trim()
         t.equal(String(data), expected, 'extracted all the CSS')
       })
+    }
+  })
+
+  t.test('should write file', function (t) {
+    t.plan(3)
+    tmpDir({unsafeCleanup: true}, onDir)
+
+    function onDir (err, dir, cleanup) {
+      t.ifError(err, 'no error')
+      const outFile = path.join(dir, 'out.css')
+
+      browserify(path.join(__dirname, 'source.js'))
+        .transform('sheetify/transform')
+        .plugin(cssExtract, { out: outFile })
+        .bundle(function (err) {
+          t.ifError(err, 'no bundle error')
+
+          const exPath = path.join(__dirname, './expected.css')
+          const expected = fs.readFileSync(exPath, 'utf8').trim()
+          const actual = fs.readFileSync(outFile, 'utf8').trim()
+          t.equal(expected, actual, 'all css written to file')
+
+          cleanup()
+        })
     }
   })
 })
